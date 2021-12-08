728a6bfe9225caf7ed22fb82285eb0d108012b2e
Tal <talzhtc@gmail.com>
2021-12-08 19:30:20 +0200

    imports

--- app/src/main/java/DataAccessLayer/RestDB.java
+++ app/src/main/java/DataAccessLayer/RestDB.java
@@ -1,8 +1,20 @@
 package DataAccessLayer;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import BusinessLogic.Item;
+import BusinessLogic.Restaurant;
 
 public class RestDB {
 
@@ -14,10 +26,20 @@ public class RestDB {
         restCollection = db.collection("restaurants");
     }
 
-    public Query getRestWithID(int id){
-        Query q = restCollection.whereEqualTo("id", id);
-        return q;
-    }
+    public void getRestaurants(List<Restaurant> restaurants){
+        restCollection.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(@NonNull QuerySnapshot queryDocumentSnapshots) {
+                        if(!queryDocumentSnapshots.isEmpty()){
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            for(DocumentSnapshot d : list){
+                                Restaurant rest = d.toObject(Restaurant.class);
+                            }
+                        }
+                    }
+                });
 
+    }
 }
 
