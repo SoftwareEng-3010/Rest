02ab1aba4e7eebf053036eea5c4da9add0057048
Tal <talzhtc@gmail.com>
2021-12-08 19:28:24 +0200

    new file

---
+++ app/src/main/java/UI/ViewRestActivity.java
@@ -0,0 +1,14 @@
+package UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewRestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_rest);
+    }
+}
