435e116ba3bbb4367019882a66efeea2f90e78f3
Tal <talzhtc@gmail.com>
2021-12-08 17:34:50 +0200

    new files

---
+++ app/src/main/java/DataAccessLayer/RestDB.java
@@ -0,0 +1,13 @@
+package DataAccessLayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestDB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
