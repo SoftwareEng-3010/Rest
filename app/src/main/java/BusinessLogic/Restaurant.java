f4127be4ab0fea38cf756f641a675899c1739b32
Tal <talzhtc@gmail.com>
2021-12-08 18:24:16 +0200

    Added getters and ctor

--- app/src/main/java/BusinessLogic/Restaurant.java
+++ app/src/main/java/BusinessLogic/Restaurant.java
@@ -1,10 +1,35 @@
 package BusinessLogic;
 
+import java.util.ArrayList;
+
 public class Restaurant {
+
     private int id;
     private int numOfBranches;
 
     private String name;
+    private ArrayList<RestBranch> branches;
 
+    public Restaurant(int id, int numOfBranches, String name, ArrayList<RestBranch> branches) {
+        this.id = id;
+        this.numOfBranches = numOfBranches;
+        this.name = name;
+        this.branches = branches;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public int getNumOfBranches() {
+        return numOfBranches;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<RestBranch> getBranches() {
+        return branches;
+    }
 }
