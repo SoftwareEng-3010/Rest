159796762b7c098e0fc0459dafd3c37c6c206e6a
Itay <itay.nextweb@gmail.com>
2021-12-10 16:18:26 +0200

    Login to RestSelector Intent

--- app/src/main/java/com/example/exercise_5/MainActivity.java
+++ app/src/main/java/com/example/exercise_5/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.exercise_5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
