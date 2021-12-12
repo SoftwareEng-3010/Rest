d7e7d9a1de42a478208ec3cc668ab97e16667640
Tal <talzhtc@gmail.com>
2021-12-12 11:32:10 +0200

    List button moves to listview activity

--- app/src/main/java/com/example/exercise_5/RestSelector.java
+++ app/src/main/java/com/example/exercise_5/RestSelector.java
@@ -1,8 +1,11 @@
 package com.example.exercise_5;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -13,12 +16,16 @@ import java.util.List;
 
 import BusinessEntities.Restaurant;
 import DataAccessLayer.RestDB;
+import UI.ViewRestActivity;
 
 public class RestSelector extends AppCompatActivity {
     private RestDB restDB;
     private List<Restaurant> restaurants;
 
     private final String TAG = "RestSelector";
+
+    private Button qrBtn;
+    private Button listBtn;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +35,9 @@ public class RestSelector extends AppCompatActivity {
         String userName = getIntent().getStringExtra("UserEmail");
         Toast.makeText(this, "Welcome, " + userName, Toast.LENGTH_SHORT).show();
 
+        qrBtn = (Button)findViewById(R.id.qr_button);
+        listBtn = (Button)findViewById(R.id.list_button);
+
         restDB = RestDB.getInstance();
         restaurants = restDB.getRestaurants();
     }
@@ -36,5 +46,17 @@ public class RestSelector extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.d(TAG, Integer.toString(restaurants.size()));
+        initListeners();
+    }
+
+    private void initListeners(){
+
+        listBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewRestIntent = new Intent(RestSelector.this, ViewRestActivity.class);
+                startActivity(viewRestIntent);
+            }
+        });
     }
 }
