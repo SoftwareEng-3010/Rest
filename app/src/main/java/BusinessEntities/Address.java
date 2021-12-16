7111d8ab142d1adbadf181a020a9eaa90736caa1
shaiBonfil <shaibonfil500@gmail.com>
2021-12-16 15:56:32 +0200

    BusinessEntities update

--- app/src/main/java/BusinessEntities/Address.java
+++ app/src/main/java/BusinessEntities/Address.java
@@ -2,5 +2,46 @@ package BusinessEntities;
 
 public class Address {
 
-    // HashMap<String, String> address
+    private String city;
+    private String street;
+    private String buildingNumber;
+
+    public Address(String city, String street, String buildingNumber) {
+        this.city = city;
+        this.street = street;
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(String buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", buildingNumber='" + buildingNumber + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/BusinessEntities/Table.java
+++ app/src/main/java/BusinessEntities/Table.java
@@ -1,4 +1,70 @@
 package BusinessEntities;
 
 public class Table {
+
+    private int tableIndex;
+    private int capacity;
+    private double billAmount;
+    private boolean isOccupied;
+    private boolean isInside;
+
+    public Table(int tableIndex, int capacity, double billAmount,
+                 boolean isOccupied, boolean isInside) {
+        this.tableIndex = tableIndex;
+        this.capacity = capacity;
+        this.billAmount = billAmount;
+        this.isOccupied = isOccupied;
+        this.isInside = isInside;
+    }
+
+    public int getTableIndex() {
+        return tableIndex;
+    }
+
+    public void setTableIndex(int tableIndex) {
+        this.tableIndex = tableIndex;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getBillAmount() {
+        return billAmount;
+    }
+
+    public void setBillAmount(double billAmount) {
+        this.billAmount = billAmount;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public boolean isInside() {
+        return isInside;
+    }
+
+    public void setInside(boolean inside) {
+        isInside = inside;
+    }
+
+    @Override
+    public String toString() {
+        return "Table{" +
+                "tableIndex=" + tableIndex +
+                ", capacity=" + capacity +
+                ", billAmount=" + billAmount +
+                ", isOccupied=" + isOccupied +
+                ", isInside=" + isInside +
+                '}';
+    }
 }
