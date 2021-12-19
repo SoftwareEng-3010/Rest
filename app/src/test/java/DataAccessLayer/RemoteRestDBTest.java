092d2b7b02c7e759dae7ee034742383fc9471fd5
aloneshed <aloneshed1@gmail.com>
2021-12-19 18:50:08 +0200

    Unit Testing RemoteRestDB

---
+++ app/src/test/java/DataAccessLayer/RemoteRestDBTest.java
@@ -0,0 +1,52 @@
+package DataAccessLayer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RemoteRestDBTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void writeRestaurantToDB() {
+    }
+
+    @Test
+    public void getRestaurantDoNotUse() {
+    }
+
+    @Test
+    public void getBranchDoNotUse() {
+    }
+
+    @Test
+    public void getRestaurant() {
+    }
+
+    @Test
+    public void getBranchAddresses() {
+    }
+
+    @Test
+    public void getOpenTables() {
+    }
+
+    @Test
+    public void getBranch() {
+    }
+
+    @Test
+    public void getBranches() {
+    }
+
+    @Test
+    public void testGetRestaurant() {
+    }
+
+    @Test
+    public void getRestaurants() {
+    }
+}
