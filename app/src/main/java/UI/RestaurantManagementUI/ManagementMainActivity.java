6c5d9f9f8ab23868fb9de38dc5f1bbf3fb1df6af
Itay <itay.nextweb@gmail.com>
2021-12-30 03:02:56 +0200

    Branch management UI implementation

--- app/src/main/java/UI/RestaurantManagementUI/ManagementMainActivity.java
+++ app/src/main/java/UI/RestaurantManagementUI/ManagementMainActivity.java
@@ -10,7 +10,6 @@ import android.widget.Toast;
 import com.example.exercise_5.R;
 
 import API.Constants.Constants;
-import DataAccessLayer.RestDB;
 import UI.DataActivity.DataActivity;
 
 public class ManagementMainActivity extends AppCompatActivity {
@@ -22,50 +21,18 @@ public class ManagementMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_management_main);
 
-        textView = (TextView) findViewById(R.id.textView);
-
         String managerUid = getIntent().getStringExtra("manager_uid");
         String branchUid = getIntent().getStringExtra("branch_uid");
         int userType = getIntent().getIntExtra("user_type", -1);
-
 
 
         if (userType == -1) {
             Toast.makeText(this, "userType == -1", Toast.LENGTH_SHORT).show();
         }
 
-        setTextOnUI(userType);
-
 //        if (branchUid == null) {
 //            Intent dataActivity = new Intent(this, DataActivity.class);
 //            startActivity(dataActivity);
 //        }
-
-    }
-
-
-
-    private void setTextOnUI(int userType) {
-        switch (userType) {
-            case Constants
-                    .USER_TYPE_BRANCH_MANAGER:
-                textView.setText("Hello, Manager!");
-                break;
-
-            case Constants
-                    .USER_TYPE_BRANCH_MANAGER2:
-                textView.setText("Hello, Second Manager!");
-                break;
-
-            case Constants
-                    .USER_TYPE_SERVICE:
-                textView.setText("Hello, Waiter / Waitress!");
-                break;
-
-            case Constants
-                    .USER_TYPE_KITCHEN:
-                textView.setText("Hello, Kitchen!");
-                break;
-        }
     }
 }
