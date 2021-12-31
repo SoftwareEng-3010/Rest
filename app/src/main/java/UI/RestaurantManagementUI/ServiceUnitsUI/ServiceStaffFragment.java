c99d481a665965f06ad5d9acacedb99276eeee6c
Itay <itay.nextweb@gmail.com>
2021-12-31 11:50:51 +0200

    Save and start working on ManagementUI

--- app/src/main/java/UI/RestaurantManagementUI/ServiceUnitsUI/ServiceStaffFragment.java
+++ app/src/main/java/UI/RestaurantManagementUI/ServiceUnitsUI/ServiceStaffFragment.java
@@ -17,6 +17,11 @@ public class ServiceStaffFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_service_staff, container, false);
+        View v = inflater.inflate(R.layout.fragment_service_staff, container, false);
+
+
+
+
+        return v;
     }
 }
