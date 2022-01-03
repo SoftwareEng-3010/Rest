b00ee7a0a0a7e76e51b6805ea11d86b96c977a80
shaiBonfil <shaibonfil500@gmail.com>
2022-01-03 23:44:31 +0200

    save

--- app/src/main/java/BusinessEntities/Printer.java
+++ app/src/main/java/BusinessEntities/Printer.java
@@ -3,8 +3,6 @@ package BusinessEntities;
 import android.content.Context;
 import android.widget.Toast;
 
-import java.util.Collection;
-
 public class Printer<T> {
 
     public void print(T object, Context context){
