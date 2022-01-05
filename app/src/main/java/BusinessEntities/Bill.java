26a87971ba5bfd0f84fe8b2dcd8bfaa0e1559cd5
Tal <talzhtc@gmail.com>
2022-01-05 22:26:43 +0200

    refactor

--- app/src/main/java/BusinessEntities/Bill.java
+++ app/src/main/java/BusinessEntities/Bill.java
@@ -3,44 +3,59 @@ package BusinessEntities;
 import java.util.ArrayList;
 import java.util.List;
 
+import API.Models.IOrder;
+
 public class Bill {
 
     private final double EMPTY_BILL = 0.0;
 
-    private List<Order> orders;
-    private double amount;
+    private List<IOrder> orders;
+    private double total;
 
     public Bill(){
         orders = new ArrayList<>();
-        double amount = EMPTY_BILL;
+        total = EMPTY_BILL;
     }
 
-    public void addOrder(Order order){
+    public Bill(Bill other){
+         this.orders = new ArrayList<>(other.getOrders());
+         this.total = other.getTotal();
+    }
+
+    public void addOrder(IOrder order){
         orders.add(order);
         for(Item item : order.getOrderItems()){
-            amount += item.getPrice();
+            total += item.getPrice();
         }
     }
 
     public void removeOrder(String orderId){
-        for (Order order : orders){
+        for (IOrder order : orders){
             if(order.getDocId() == orderId){
                 orders.remove(order);
                 for(Item item : order.getOrderItems()){
-                    amount -= item.getPrice();
+                    total -= item.getPrice();
                 }
             }
         }
     }
 
+    public double getTotal(){
+        return this.total;
+    }
+
+    public List<IOrder> getOrders(){
+        return this.orders;
+    }
+
     public String toString(){
         String res = "";
-        for (Order order : orders){
+        for (IOrder order : orders){
             for(Item item : order.getOrderItems()){
                 res += item.toString() + '\n';
             }
         }
-        res += "Total Amount:\t" + Double.toString(amount);
+        res += "Total Amount:\t" + Double.toString(total);
         return res;
     }
 }
