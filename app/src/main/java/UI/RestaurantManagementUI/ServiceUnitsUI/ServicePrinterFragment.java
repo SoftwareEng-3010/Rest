3066a6ddb213fd989de37a273f0fb4149e1d059a
Tal <talzhtc@gmail.com>
2022-01-06 16:25:57 +0200

    new files

---
+++ app/src/main/java/UI/RestaurantManagementUI/ServiceUnitsUI/ServicePrinterFragment.java
@@ -0,0 +1,23 @@
+package UI.RestaurantManagementUI.ServiceUnitsUI;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.exercise_5.R;
+
+public class ServicePrinterFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_service_printer, container, false);
+
+        return v;
+    }
+}
